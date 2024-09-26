69f28caa4096e81dde92b04189e95fba7b3e0c5d
Piyushzingade <piyushzingade@gmail.com>
2024-09-26 11:33:32 +0530

    Added bg

---
+++ src/components/FloatingShape.tsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion"
+
+
+interface FloatingShapeProps {
+    color :string,
+    size:string,
+    top: string,
+    left :string
+    delay:number
+}
+
+export const FloatingShape = ({color , size , top, left, delay} : FloatingShapeProps)=>{
+    return <motion.div className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl  `}
+    style={{top , left}}
+    animate={{
+        y:["0%" , "100%" ,"0%"],
+        x:["0%" , "100%" ,"0%"],
+        rotate : [0,360]
+    }}
+    transition={{
+        duration:20,
+        ease:"linear",
+        repeat:Infinity,
+        delay
+    }}
+    aria-hidden = "true"
+    />
+}
